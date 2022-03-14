b93323e1e254ecf3d4769d1334028bbb109f13e9
ahmedha31 <52720071+ahmedha31@users.noreply.github.com>
2022-03-14 20:54:19 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+require("dotenv").config();
+const Database = require("@replit/database")
+const db = new Database()
+const client = new Discord.Client();
+const config = {
+  prefix: "4"
+};
+//////////////////////////////
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(3000);
+setInterval(() => {
+  http.get(`https://Discordjs-MusicBot-4.ahmedha31.repl.co`);
+  console.log("refrsh")
+}, 060000);
+/////////////////////////////
+client.config = config;
+client.queue = new Map();
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`[Command Manager]: Loading Command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
+
+
+
+db.get("token").then(value => {client.login(value);});
