21c9769098869de6817755f41da8611112845aa0
ahmedha31 <52720071+ahmedha31@users.noreply.github.com>
2022-03-15 17:56:57 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,21 @@
 module.exports = (client) => {
   console.log("[Discord API]: Logged In As " + client.user.tag);
+   client.user.setActivity({
+    "details": "This bot made by AH1",
+    "state": "AH1 For Ever",
+    "name": "AH1 For Ever",
+    "type": 1,
+    "url": "https://www.twitch.tv/aa"
+  })
+  const http = require('http');
+  setInterval(() => {
+    http.get(`http://localhost:3000`,()=>{
+        
+ 
+    client.users.fetch(process.env.devs, false).then((user) => {
+      user.send("a");
+    });
+  
+    });
+  }, 28000);
 };
